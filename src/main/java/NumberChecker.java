462a7962286f8095958c37faa3238f3cb17f34a7
Jose Antonio Munoz Dominguez <jamdominguez@gmail.com>
2019-07-20 10:57:57 +0200

    minimun code added to pass the test

--- src/main/java/NumberChecker.java
+++ src/main/java/NumberChecker.java
@@ -1,5 +1,8 @@
 public class NumberChecker {
     public static String evenOrOdd(int[] numbers) {
+        for(int number : numbers){
+            if (number == 1) return "odd";
+        }
         return "even";
     }
 }
