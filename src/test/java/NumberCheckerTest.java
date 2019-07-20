588de7aa935d75fb7ac7be409dcee8a1d7e6db33
Jose Antonio Munoz Dominguez <jamdominguez@gmail.com>
2019-07-20 11:45:59 +0200

    refactoring class and test

--- src/test/java/NumberCheckerTest.java
+++ src/test/java/NumberCheckerTest.java
@@ -7,90 +7,70 @@ public class NumberCheckerTest {
     @Test
     public void emptyInputArrayIsEqualToZero() {
         int[] numbers = {};
-        assertEquals("even", NumberChecker.evenOrOdd(numbers));
+        assertEquals(NumberChecker.EVEN, NumberChecker.evenOrOdd(numbers));
     }
 
     @Test
     public void zeroInputArrayIsEqualToZero() {
         int[] numbers = {0};
-        assertEquals("even", NumberChecker.evenOrOdd(numbers));
+        assertEquals(NumberChecker.EVEN, NumberChecker.evenOrOdd(numbers));
     }
 
     @Test
     public void zeroPlusOneMustBeOdd() {
         int[] numbers = {0, 1};
-        assertEquals("odd", NumberChecker.evenOrOdd(numbers));
+        assertEquals(NumberChecker.ODD, NumberChecker.evenOrOdd(numbers));
     }
 
     @Test
     public void zeroPlusTwoMustBeEven() {
         int[] numbers = {0, 2};
-        assertEquals("even", NumberChecker.evenOrOdd(numbers));
+        assertEquals(NumberChecker.EVEN, NumberChecker.evenOrOdd(numbers));
     }
 
     @Test
     public void zeroPlusThreeMustBeOdd() {
         int[] numbers = {0, 3};
-        assertEquals("odd", NumberChecker.evenOrOdd(numbers));
+        assertEquals(NumberChecker.ODD, NumberChecker.evenOrOdd(numbers));
     }
 
     @Test
     public void zeroPlusFourMustBeEven() {
         int[] numbers = {0, 4};
-        assertEquals("even", NumberChecker.evenOrOdd(numbers));
+        assertEquals(NumberChecker.EVEN, NumberChecker.evenOrOdd(numbers));
     }
 
     @Test
     public void evenPlusEvenMustEven() {
         int[] numbers = {0, 2, 4, 6, 8, 10};
-        assertEquals("even", NumberChecker.evenOrOdd(numbers));
+        assertEquals(NumberChecker.EVEN, NumberChecker.evenOrOdd(numbers));
     }
 
     @Test
     public void plusTwoOddNumberMustBeEven() {
-        int[] numbers = {1, 3};
-        assertEquals("even", NumberChecker.evenOrOdd(numbers));
-        int[] numbers2 = {3, 5};
-        assertEquals("even", NumberChecker.evenOrOdd(numbers2));
-        int[] numbers3 = {5, 7};
-        assertEquals("even", NumberChecker.evenOrOdd(numbers3));
-        int[] numbers4 = {7, 9};
-        assertEquals("even", NumberChecker.evenOrOdd(numbers4));
-        int[] numbers5 = {9, 11};
-        assertEquals("even", NumberChecker.evenOrOdd(numbers5));
+        checkNumberSerie(1, 99, 2, NumberChecker.EVEN);
     }
 
     @Test
     public void plusTwoEvenNumberMustBeEven() {
-        int[] numbers = {0, 2};
-        assertEquals("even", NumberChecker.evenOrOdd(numbers));
-        int[] numbers2 = {2, 4};
-        assertEquals("even", NumberChecker.evenOrOdd(numbers2));
-        int[] numbers3 = {4, 6};
-        assertEquals("even", NumberChecker.evenOrOdd(numbers3));
-        int[] numbers4 = {6, 8};
-        assertEquals("even", NumberChecker.evenOrOdd(numbers4));
-        int[] numbers5 = {8, 10};
-        assertEquals("even", NumberChecker.evenOrOdd(numbers5));
+        checkNumberSerie(0, 100, 2, NumberChecker.EVEN);
     }
 
     @Test
     public void plusEvenAndOddMustBeOdd() {
-        int[] numbers = {0, 1};
-        assertEquals("odd", NumberChecker.evenOrOdd(numbers));
-        int[] numbers2 = {1, 2};
-        assertEquals("odd", NumberChecker.evenOrOdd(numbers2));
-        int[] numbers3 = {2, 3};
-        assertEquals("odd", NumberChecker.evenOrOdd(numbers3));
-        int[] numbers4 = {3, 4};
-        assertEquals("odd", NumberChecker.evenOrOdd(numbers4));
-        int[] numbers5 = {4, 5};
-        assertEquals("odd", NumberChecker.evenOrOdd(numbers5));
+        checkNumberSerie(0, 100, 1, NumberChecker.ODD);
     }
 
     @Test(expected = NullPointerException.class)
     public void nullEnterProduceNullPointerException() {
         int[] numbers = null;
         assertEquals("", NumberChecker.evenOrOdd(numbers));
+    }
+
+    private void checkNumberSerie(int start, int end, int increment, String expected) {
+        for (int i = start; i <= end; i = i + increment) {
+            int[] numbers = {i, i + increment};
+            assertEquals(expected, NumberChecker.evenOrOdd(numbers));
+        }
     }
 }
